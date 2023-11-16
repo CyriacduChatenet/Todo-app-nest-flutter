c4cb9e314c14dd780a7a2dd342051316bf2038f0
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-11-16 11:39:16 +0100

    feat(client): add auth router

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:client/blocs/todos/todos_bloc.dart';
 import 'package:client/blocs/todos/todos_event.dart';
@@ -20,30 +21,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RepositoryProvider(
-        create: (context) => TodoRepository(),
-        child: MultiBlocProvider(
-          providers: [
-            BlocProvider(
-                create: (context) => TodosBloc(
-                      RepositoryProvider.of<TodoRepository>(context),
-                    )..add(LoadTodo()))
-          ],
-          child: MaterialApp(
-            title: 'Flutter Demo',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-              useMaterial3: true,
-            ),
-            initialRoute: '/signin',
-            routes: {
-              '/signin': (context) => const SigninScreen(),
-              '/signup': (context) => const SignupScreen(),
-              '/forgot-password': (context) => const ForgotPasswordScreen(),
-              '/': (context) => const HomeScreen(),
-              '/profile': (context) => const ProfileScreen(),
+      create: (context) => TodoRepository(),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => TodosBloc(
+              RepositoryProvider.of<TodoRepository>(context),
+            )..add(const LoadTodo()),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            useMaterial3: true,
+          ),
+          home: FutureBuilder(
+            // Check if the token is present
+            future: const FlutterSecureStorage().read(key: 'token'),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // If token is present, navigate to HomeScreen, else to SigninScreen
+                final String? token = snapshot.data;
+                final String initialRoute =
+                    token != null && token.isNotEmpty ? '/' : '/signin';
+
+                return MaterialApp(
+                  initialRoute: initialRoute,
+                  routes: {
+                    '/signin': (context) => const SigninScreen(),
+                    '/signup': (context) => const SignupScreen(),
+                    '/forgot-password': (context) =>
+                        const ForgotPasswordScreen(),
+                    '/': (context) => const HomeScreen(),
+                    '/profile': (context) => const ProfileScreen(),
+                  },
+                  debugShowCheckedModeBanner: false,
+                );
+              } else {
+                // Show a loading screen while checking for the token
+                return const CircularProgressIndicator();
+              }
             },
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
