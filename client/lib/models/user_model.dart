ede21a4022f1303a729585b997f6d7e10a543f4f
CyriacduChatenet <cyriacduchatenet@icloud.com>
2023-11-18 11:50:46 +0100

    fix(client): change user model

--- client/lib/models/user_model.dart
+++ client/lib/models/user_model.dart
@@ -30,9 +30,7 @@ class User {
       username: json['username'] as String?,
       email: json['email'] as String? ?? '',
       password: json['password'] as String? ?? '',
-      todos: (json['todos'] as List<dynamic>)
-          .map((todoJson) => Todo.fromJson(todoJson as Map<String, Object?>))
-          .toList(),
+      todos: (json['todos'] as List<dynamic>?)?.map((todoJson) => Todo.fromJson(todoJson as Map<String, Object?>)).toList() ?? [],
     );
   }
 
